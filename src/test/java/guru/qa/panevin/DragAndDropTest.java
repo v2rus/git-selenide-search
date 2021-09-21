99eaf11c4ac0877c3446181a87f4eaab4c7449ee
Nikolay Panevin <Nikolay.Panevin@dataart.com>
2021-09-21 17:31:15 +0300

    drag and drop test added

---
+++ src/test/java/guru/qa/panevin/DragAndDropTest.java
@@ -0,0 +1,18 @@
+package guru.qa.panevin;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class DragAndDropTest {
+    @Test
+    void dragAndDropAtoB (){
+        Configuration.startMaximized = true;
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
+    }
+}
--- src/test/java/guru/qa/panevin/SelenideSerachTest.java
+++ src/test/java/guru/qa/panevin/SelenideSerachTest.java
@@ -2,7 +2,6 @@ package guru.qa.panevin;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
