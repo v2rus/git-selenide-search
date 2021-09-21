f6e3e833ad46a4c8643223668e7247ff5b788ffb
Nikolay Panevin <Nikolay.Panevin@dataart.com>
2021-09-21 17:20:04 +0300

    project setup. SoftAssertions and JUnit5 test added

---
+++ src/test/java/guru/qa/panevin/SelenideSerachTest.java
@@ -0,0 +1,26 @@
+package guru.qa.panevin;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class SelenideSerachTest {
+
+    @Test
+    void softAssecrtionAndJUnitExistTest () throws InterruptedException {
+        Configuration.startMaximized = true;
+        open ("https://github.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[href=\"/selenide/selenide\"]").click();
+        $("[data-content=\"Wiki\"]").click();
+        $("#wiki-body").shouldHave(Condition.text("Soft assertions"));
+        $("#wiki-body").$(byText("Soft assertions")).click();
+        $("#wiki-body").shouldHave(Condition.text("Using JUnit5 extend test class:"));
+    }
+
+}
